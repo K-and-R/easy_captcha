dcef4fc7bf3dd6446260862bf407b991c3f77351
gropher <grophen@gmail.com>
2017-03-13 15:17:23 +0300

    Routes fixed

--- lib/easy_captcha/routes.rb
+++ lib/easy_captcha/routes.rb
@@ -3,7 +3,7 @@ module ActionDispatch #:nodoc:
     class Mapper #:nodoc:
       # call to add default captcha root
       def captcha_route
-        match 'captcha' => EasyCaptcha::Controller, :action => :captcha, :via => :get, :as => :captcha_path
+        match 'captcha' => EasyCaptcha::Controller, :action => :captcha, :via => :get, :as => :captcha
       end
     end
   end
