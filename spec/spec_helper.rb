e622ad5e048a6b368acf2062efbdad683eea38d4
Karl Wilbur <karl@kandrsoftware.com>
2020-11-15 14:11:24 -0500

    Update RSpec tests

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,12 +1,13 @@
+# frozen_string_literal: true
+
+require 'pry'
+
 $LOAD_PATH.unshift(File.join(File.dirname(__FILE__), '..', 'lib'))
 $LOAD_PATH.unshift(File.dirname(__FILE__))
-begin
-  require 'simplecov'
-  SimpleCov.start 'rails'
-rescue LoadError
-end
+require 'simplecov'
+SimpleCov.start 'rails'
 require 'easy_captcha'
 
 # Requires supporting files with custom matchers and macros, etc,
 # in ./support/ and its subdirectories.
-Dir["#{File.dirname(__FILE__)}/support/**/*.rb"].each { |f| require f }
+Dir["#{File.dirname(__FILE__)}/support/**/*.rb"].sort.each { |f| require f }
